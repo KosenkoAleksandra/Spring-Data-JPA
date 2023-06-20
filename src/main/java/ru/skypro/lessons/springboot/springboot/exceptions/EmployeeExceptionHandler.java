cd61633ce0b5cda8271a23a4eb521f66204b4c23
KosenkoAleksandra <kosenkoyes@gmail.com>
2023-06-20 20:25:08 +0300

    Логирование и конфигурирование приложения.

--- src/main/java/ru/skypro/lessons/springboot/springboot/exceptions/EmployeeExceptionHandler.java
+++ src/main/java/ru/skypro/lessons/springboot/springboot/exceptions/EmployeeExceptionHandler.java
@@ -1,37 +1,49 @@
 package ru.skypro.lessons.springboot.springboot.exceptions;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
 
 import java.io.IOException;
 import java.sql.SQLException;
 
 @RestControllerAdvice
 public class EmployeeExceptionHandler {
+
+    private static final Logger logger = LoggerFactory.getLogger(EmployeeExceptionHandler.class);
+
     @ExceptionHandler
     public ResponseEntity<?> handleIOException(IOException ioException) {
+        logger.error(ioException.getMessage(), ioException);
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
     @ExceptionHandler
     public ResponseEntity<?> handleException(Exception exception) {
+        logger.error(exception.getMessage(), exception);
         return new ResponseEntity<>(HttpStatus.FORBIDDEN);
     }
     @ExceptionHandler
     public ResponseEntity<?> handleSQLException(SQLException sqlException) {
+        logger.error(sqlException.getMessage(), sqlException);
         return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
     }
     @ExceptionHandler(ReportNotFoundException.class)
-    public ResponseEntity<?> notFound() {
+    public ResponseEntity<?> notFound(RuntimeException e) {
+        logger.error(e.getMessage(), e);
         return ResponseEntity.notFound().build();
     }
     @ExceptionHandler(IllegalJsonFileException.class)
-    public ResponseEntity<?> badRequest() {
+    public ResponseEntity<?> badRequest(IllegalJsonFileException e) {
+        logger.error(e.getMessage(), e);
         return ResponseEntity.badRequest().build();
     }
     @ExceptionHandler(InternalServerError.class)
-    public ResponseEntity<?> internalServerError() {
+    public ResponseEntity<?> internalServerError(InternalServerError e) {
+        logger.error(e.getMessage(), e);
         return ResponseEntity.internalServerError().build();
     }
 }
--- src/main/java/ru/skypro/lessons/springboot/springboot/service/EmployeeServiceImpl.java
+++ src/main/java/ru/skypro/lessons/springboot/springboot/service/EmployeeServiceImpl.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Data;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
 import org.springframework.data.domain.Page;
@@ -31,29 +33,36 @@ import java.util.stream.Collectors;
 @Data
 @Service
 public class EmployeeServiceImpl implements EmployeeService{
+
+    private static final Logger logger = LoggerFactory.getLogger(EmployeeServiceImpl.class);
     private final EmployeeRepository employeeRepository;
     private final ObjectMapper objectMapper;
     private final ReportRepository reportRepository;
 
     @Override
     public List<EmployeeDTO> getAllEmployees() {
+        logger.debug("getAllEmployees method was invoked");
         return employeeRepository.findAllEmployees().stream()
                 .map(EmployeeDTO::fromEmployee)
                 .collect(Collectors.toList());
+
     }
 
     @Override
     public EmployeeDTO addEmployee(Employee employee) {
+        logger.debug("addEmployee method was invoked with parameter: {}", employee);
         return EmployeeDTO.fromEmployee(employeeRepository.save(employee));
     }
     @Override
     public void addBatchEmployees(List<EmployeeDTO> employees) {
+        logger.debug("addBatchEmployees method was invoked with parameter: {}", employees);
         employees.stream()
                 .map(EmployeeDTO::toEmployee)
                 .forEach(employeeRepository::save);
     }
     @Override
     public EmployeeDTO getEmployeeById(int id) {
+        logger.debug("getEmployeeById method was invoked with parameter: {}", id);
         return EmployeeDTO.fromEmployee(
                 employeeRepository.findById(id)
                         .orElse(new Employee())
@@ -61,29 +70,35 @@ public class EmployeeServiceImpl implements EmployeeService{
     }
     @Override
     public void deleteEmployeeById(int id) {
+        logger.debug("deleteEmployeeById method was invoked with parameter: {}", id);
         employeeRepository.deleteById(id);
     }
     @Override
     public List<EmployeeDTO> getEmployeesByName (String name) {
+        logger.debug("getEmployeesByName method was invoked with parameter: {}", name);
         return employeeRepository.getEmployeesByName(name);
     }
     @Override
     public List<EmployeeFullInfo> getAllInfo() {
+        logger.debug("getAllInfo method was invoked");
         return employeeRepository.findAllEmployeeFullInfo();
     }
 
     @Override
     public EmployeeDTO employeeWithHighestSalary() {
+        logger.debug("employeeWithHighestSalary method was invoked");
         return EmployeeDTO.fromEmployee(employeeRepository.employeeWithHighestSalary());
     }
     @Override
     public List<EmployeeDTO> allEmployeesByPosition(String name) {
+        logger.debug("allEmployeesByPosition method was invoked with parameter: {}", name);
         return employeeRepository.returnAllByPositionId(name).stream()
                 .map(EmployeeDTO::fromEmployee)
                 .collect(Collectors.toList());
     }
     @Override
     public List<Employee> getEmployeeWithPaging(int pageIndex, int unitPerPage) {
+        logger.debug("getEmployeeWithPaging method was invoked with parameters: {}, {}", pageIndex, unitPerPage);
         Pageable employeeOfConcretePage = PageRequest.of(pageIndex, unitPerPage);
         Page<Employee> page = employeeRepository.findAll(employeeOfConcretePage);
 
@@ -92,6 +107,7 @@ public class EmployeeServiceImpl implements EmployeeService{
     }
     @Override
     public void upload(MultipartFile employees) {
+        logger.debug("upload method was invoked with fileName: {}", employees.getOriginalFilename());
         try {
             String extension = StringUtils.getFilenameExtension(employees.getOriginalFilename());
             if (!"json".equals(extension)) {
@@ -104,26 +120,31 @@ public class EmployeeServiceImpl implements EmployeeService{
             );
             addBatchEmployees(employeeDTOS);
         } catch (IOException e) {
+            logger.error(e.getMessage(), e);
             throw new IllegalJsonFileException();
         }
     }
     @Override
     public int createReport() {
+        logger.debug("createReport method was invoked");
         try {
             Report report = new Report();
             report.setReport(buildReport());
             return reportRepository.save(report).getId();
         } catch (JsonProcessingException e) {
+            logger.error(e.getMessage(), e);
             throw new InternalServerError();
         }
     }
     public String buildReport() throws JsonProcessingException {
+        logger.debug("buildReport method was invoked");
       List<ReportDTO> reports = employeeRepository.buildReports();
       return objectMapper.writeValueAsString(reports);
     }
 
     @Override
     public Resource downloadReport(int id) {
+        logger.debug("downloadReport method was invoked with parameter: {}",id);
         return new ByteArrayResource(
                 reportRepository.findById(id)
                         .orElseThrow(ReportNotFoundException::new)
